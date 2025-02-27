7eb01c6a5052f368448288b0f408274616f3ff6f
MXMaddux <maxmaddux@gmail.com>
2025-02-27 02:45:52 -0800

    Configure Clerk

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+"use client";
+import { toast } from "sonner";
+import { SignOutButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 function SignOutLink() {
-  return <div>SignOutLink</div>;
+  const handleLogout = () => {
+    toast("Logout Successful");
+  };
+
+  return (
+    <SignOutButton>
+      <Link href={"/"} className="w-full text-left" onClick={handleLogout}>
+        Logout
+      </Link>
+    </SignOutButton>
+  );
 }
 
 export default SignOutLink;
--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { LucideUser2 } from "lucide-react";
+import { currentUser } from "@clerk/nextjs/server";
 
-function UserIcon() {
-  return <div>UserIcon</div>;
+async function UserIcon() {
+  const user = await currentUser();
+
+  const profileImage = user?.imageUrl;
+
+  if (profileImage) {
+    return (
+      <img
+        src={profileImage}
+        alt="profile image"
+        className="w-6 h-6 rounded-full object-cover"
+      />
+    );
+  }
+
+  return <LucideUser2 className="w-6 h-6 bg-primary rounded-full text-white" />;
 }
 
 export default UserIcon;
