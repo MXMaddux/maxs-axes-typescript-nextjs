f1b98bf2c3db6b88546115f6236714a3873df7d1
MXMaddux <maxmaddux@gmail.com>
2025-03-03 05:11:41 -0800

    Fix run dev errors

--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,20 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-// Define route matchers for public and admin routes
 const isPublicRoute = createRouteMatcher(["/", "/products(.*)", "/about"]);
 const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
-  try {
-    // Await the auth() function to get the ClerkMiddlewareAuthObject
-    const authObject = await auth();
-    const userId = authObject.userId; // Get the user ID
-    const isAdminUser = userId === process.env.ADMIN_USER_ID;
+  // if (isAdminRoute(req)) await auth.protect()
+  const isAdminUser = (await auth().userId) === process.env.ADMIN_USER_ID;
 
-    // Redirect non-admin users trying to access admin routes
-    if (isAdminRoute(req) && !isAdminUser) {
-      return NextResponse.redirect(new URL("/", req.url));
-    }
-
-    // Protect non-public routes by checking if the user is authenticated
-    if (!isPublicRoute(req) && !userId) {
-      return NextResponse.redirect(new URL("/sign-in", req.url)); // Redirect to sign-in page
-    }
-  } catch (error) {
-    console.error("Middleware Error:", error);
-
-    // Redirect to home page or show an error response
+  if (isAdminRoute(req) && !isAdminUser) {
     return NextResponse.redirect(new URL("/", req.url));
   }
+
+  if (!isPublicRoute(req)) auth.protect();
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"], // Match all routes except static files
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
